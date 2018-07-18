c323c6d21ce496ca4d60579b4609e66fa32c5fc4
眼镜哥哥v587 <327453661@qq.com>
2018-07-18 11:07:40 +0800

    Update main.js

--- UI-UE/js/main.js
+++ UI-UE/js/main.js
@@ -33,17 +33,7 @@ function doMove(obj,attr,target,speed,callBack){
         }
     },30);
 }
-function addZero(n){
-    return n < 10 ? "0"+n : n;
-}
-function getTimes(){
-    var d = new Date();
-    var H = d.getHours();
-    var Min = d.getMinutes();
-    var S = d.getSeconds();
-    var str =addZero(H)+":"+addZero(Min)+":"+addZero(S);
-    return str;
-}
+
 window.onload=function(){
     var picList=$("#list");
     var aImg=$("img",picList);
@@ -64,29 +54,6 @@ window.onload=function(){
         },2000);
     }
     play();
-    var getTime=document.getElementById("gettime");
-    var allSpan = getTime.getElementsByTagName("span");
-    var futureTime = new Date(2018,12,31,24,0,0);
-        setInterval(function (){
-            var now = new Date();
-            var t = (futureTime.getTime() - now.getTime())/1000;
-            if( t <0 ){
-                alert("时间到了");
-            }
-            var D =  Math.floor(t/86400);
-            var H =  Math.floor(t%86400/3600);
-            var Min =  Math.floor(t%86400%3600/60);
-            var S = Math.floor(t%60);
-            var str =addZero(D)+"-"+addZero(H)+"-"+addZero(Min)+"-"+addZero(S);
-            var newTime=str.split("-");
-            var time=newTime.join("");
-            for( var i = 0; i < time.length; i++ ){
-                if(time.charAt(i) !=""){
-                    allSpan[i].innerHTML = time.charAt(i);
-                }
-
-            }
-        },1000)
     var btn=$("#btn");
     var aSpan=$("span",btn);
     aSpan[0].onmouseover=function(){
